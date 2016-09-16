42d58fd044531d93385e062e9113de67a536bd05
boididou <boididou@iti.gr>
2016-09-16 16:08:42 +0300

    Evaluation script updated
    Evaluation script updated to include tampered/non-tampered (subtask)
    case.

--- mediaeval2016/src/evaluationScript/EvaluationScript.java
+++ mediaeval2016/src/evaluationScript/EvaluationScript.java
@@ -63,6 +63,66 @@ public class EvaluationScript {
 		System.out.println("F-Score: "	 + fScore);
 	}
 	
+	public static void evaluateSubtask(HashMap<String,String> map_IdsLabels_groundtruth, HashMap<String,String> map_IdsLabels_predicted) {
+		
+		int counterPredictionsWithValidLabels = 0;
+		
+		int truePositives = 0;
+		int trueNegatives = 0;
+		int falsePositives = 0;
+		int falseNegatives = 0;
+		
+		int sumPositiveInstances = 0;
+		
+		double precision = 0.0;
+		double recall = 0.0;
+		double fScore = 0.0;
+		
+		
+		Iterator iter0 = map_IdsLabels_groundtruth.entrySet().iterator();
+		while (iter0.hasNext()) {
+			Map.Entry mEntry = (Map.Entry) iter0.next();
+			
+			if (mEntry.getValue().equals("tampered")) {
+				sumPositiveInstances++;
+			}
+		}
+		
+		Iterator iter = map_IdsLabels_predicted.entrySet().iterator();
+		 
+		while (iter.hasNext()) {
+			Map.Entry mEntry = (Map.Entry) iter.next();
+			String predicted = mEntry.getValue().toString();
+			String actual 	 = map_IdsLabels_groundtruth.get(mEntry.getKey());
+			
+			
+			if (predicted.equals("tampered")) {
+				counterPredictionsWithValidLabels++;
+				if (predicted.equals(actual)) truePositives++;
+				else falsePositives++;	
+				if (!predicted.equals(actual)) {
+					System.out.println(mEntry.getKey());
+				}
+			}
+			else if (predicted.equals("non-tampered")){
+				counterPredictionsWithValidLabels++;
+				if (predicted.equals(actual)) trueNegatives++;
+				else falseNegatives++;	
+			}
+		}
+		System.out.println("Accuracy: "+ (double)(truePositives+trueNegatives)/map_IdsLabels_predicted.size());
+		System.out.println(sumPositiveInstances);
+		precision = (double) truePositives / (truePositives + falsePositives);
+		recall	  = (double) truePositives / (sumPositiveInstances);
+		fScore	  = (double) 2*precision*recall / (precision+recall);
+		
+		System.out.println(counterPredictionsWithValidLabels+" out of "+map_IdsLabels_groundtruth.size() +" tweets predicted as tampered or non-tampered.");
+		System.out.println("Recall: "	 + recall);
+		System.out.println("Precision: " + precision);
+		System.out.println("F-Score: "	 + fScore);
+		
+	}
+	
 	public static HashMap<String, String> loadHashmapfromFile(String file)
 			throws IOException {
 		// load hashmap from file
